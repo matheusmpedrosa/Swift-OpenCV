0eaf834b96f477ffc8efff6dcd17546da80993d4
Matheus Pedrosa <math.pedrosa@hotmail.com>
2019-02-14 14:27:17 -0300

    buttons to open camera and photo library, UIImageView to receive the image

--- Swift-OpenCV/ViewController.swift
+++ Swift-OpenCV/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnCamera: UIBarButtonItem!
+    @IBOutlet weak var btnLibrary: UIBarButtonItem!
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,5 +21,34 @@ class ViewController: UIViewController {
         print(SwiftOpenCVWrapper.openCVVersionString())
         //opencv2.framework at .gitignore
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let imagePicked = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        image.image = imagePicked
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func openCamera(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func openLibrary(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
 }
 
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+}
