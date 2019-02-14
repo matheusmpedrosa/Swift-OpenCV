469fcf30c012ad4dda642d924fb0cdcf4a3069ef
Matheus Malcher Pedrosa <matheusmpedrosa@matheuss-macbook-pro.local>
2019-02-14 10:36:36 -0300

    Initial Commit

---
+++ Swift-OpenCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-OpenCV
+//
+//  Created by Matheus Pedrosa on 2/14/19.
+//  Copyright © 2019 Matheus Pedrosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
